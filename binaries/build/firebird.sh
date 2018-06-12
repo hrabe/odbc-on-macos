d43417a591fbec9378daf7e8fb59fd900c010583
Heiko Rabe <heiko.rabe@invision.de>
2018-06-12 12:53:22 +0200

    Hide directory change in build steps

--- binaries/build/firebird.sh
+++ binaries/build/firebird.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-pushd $1/OdbcJdbc/Builds/Gcc.darwin
+pushd $1/OdbcJdbc/Builds/Gcc.darwin > /dev/null
 sed -i '' 's/ODBCMANAGER=iODBC/ODBCMANAGER=ODBC/' makefile.darwin
 sed  -i '' 's/--def $(ODBCJDBCDEFFILE)/ /' makefile.darwin
 make -B -f makefile.darwin all
-popd
+popd > /dev/null
--- binaries/build/mysql.sh
+++ binaries/build/mysql.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-pushd $1/mysql-connector-odbc-5.3.10-src
+pushd $1/mysql-connector-odbc-5.3.10-src > /dev/null
 mkdir Release_x86_64
 cd Release_x86_64
 cmake -DWITH_UNIXODBC=1 -DODBC_LIB_DIR=/usr/local/Cellar/unixodbc/2.3.6/lib -DODBC_INCLUDES=/usr/local/Cellar/unixodbc/2.3.6/include -DMYSQL_LIBRARIES='mysql_sys;mysql_strings;mysqlclient' -DMYSQL_CONFIG_EXECUTABLE= ..
 make myodbc5a
 make myodbc5w
-popd
+popd > /dev/null
