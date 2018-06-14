f575e80be28c100bc69f16203174837b25ddef9a
Heiko Rabe <heiko.rabe@invision.de>
2018-06-14 02:47:31 +0200

    Upgrade Firebird to 3.0.3

--- binaries/app/FirebirdCS-2.5.8-27089-1-x86_64.install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-SCRIPTPATH=$(cd "$(dirname "$0")"; pwd)
-echo "Unattended install of: $1"
-installer -allowUntrusted -verboseR -pkg "$SCRIPTPATH/$1" -target /
-echo "Stop and prevent Launchd"
-launchctl unload -w /Library/Frameworks/Firebird.framework/Versions/A/Resources/org.firebird.gds.plist
-
