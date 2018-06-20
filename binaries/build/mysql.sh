0c83877ad6ca6ca1855430a4cde9e30bda425535
Heiko Rabe <heiko.rabe@invision.de>
2018-06-20 17:59:11 +0200

    Freeze mysql version to get compilable odbc driver

--- binaries/build/mysql.sh
+++ binaries/build/mysql.sh
@@ -2,7 +2,15 @@
 pushd $1/mysql-connector-odbc-5.3.10-src > /dev/null
 mkdir Release_x86_64
 cd Release_x86_64
-cmake -DWITH_UNIXODBC=1 -DODBC_LIB_DIR=/usr/local/Cellar/unixodbc/2.3.6/lib -DODBC_INCLUDES=/usr/local/Cellar/unixodbc/2.3.6/include -DMYSQL_LIBRARIES='mysql_sys;mysql_strings;mysqlclient' -DMYSQL_CONFIG_EXECUTABLE= ..
+
+cmake \
+  -DWITH_UNIXODBC=1 \
+  -DODBC_LIB_DIR=/usr/local/Cellar/unixodbc/2.3.6/lib \
+  -DODBC_INCLUDES=/usr/local/Cellar/unixodbc/2.3.6/include \
+  -DMYSQL_LIB_DIR=/usr/local/opt/mysql@5.7/lib \
+  -DMYSQL_INCLUDE_DIR=/usr/local/opt/mysql@5.7/include/mysql \
+  -DMYSQL_LIBRARIES='mysql_sys;mysql_strings;mysqlclient' \
+  -DMYSQL_CONFIG_EXECUTABLE= ..
 make myodbc5a
 make myodbc5w
 popd > /dev/null
